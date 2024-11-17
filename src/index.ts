7d102bf6f530fedb0665020b5c6fad15d6024d0a
senyoudev <younesmeskafe2018@gmail.com>
2024-11-17 04:01:44 +0100

    feat: exporting stuff

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,17 @@
+/**
+ * HTML to Text Converter
+ * @packageDocumentation
+ */
+
 import { HtmlToText } from './converters/htmlToText';
 
-const converter = new HtmlToText({
-    debugMode: true,
-});
+export type {
+  ConversionOptions,
+  uppercase,
+  lowercase,
+  capitalize,
+  rowsMode,
+  cellsMode,
+} from './lib/types';
 
-const html = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Example Page</title>
-    <meta name="description" content="This is an example page."/>
-  </head>
-  <body>
-    <div id="main">
-      <p>Hello, World!</p>
-      <a href="https://example.com">Example</a>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
-      <ol>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <ul>
-            <li>Subitem 1</li>
-            <li>Subitem 2</li>
-            <li>Subitem 3</li>
-        </ul>
-      </ol>
-    </div>
-    <div id="hero">
-        <img src="hero.jpg" alt="Hero Image"/>
-    </div>
-    <div id="table">
-        <table>
-            <thead>
-                <tr>
-                    <th>Header 1</th>
-                    <th>Header 2</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Row 1, Cell 1</td>
-                    <td>Row 1, Cell 2</td>
-                </tr>
-                <tr>
-                    <td>Row 2, Cell 1</td>
-                    <td>Row 2, Cell 2</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div id='custom-replacements'>
-        <p>JavaScript code costs $1000.50</p>
-        <p>This is important warning text</p>
-        <p>Date: 25/12/2023</p>
-        <p>Temperature: 20°C</p>
-        <p>Time: 2:30pm</p>
-        <p>Phone: 1234567890</p>
-  </div>
-  </body>
-  </html>
-`;
-
-const text = converter.convert(html);
+export default HtmlToText;
